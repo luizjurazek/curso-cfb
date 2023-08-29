dcf4b72428040269fc3982c180153bfa0f4fe16d
Luiz Jurazek <luizjurazek@gmail.com>
2023-08-29 20:51:27 -0300

    Biblioteca Math

---
+++ 80 - Math/script.js
@@ -0,0 +1,7 @@
+const mat = document.getElementById("mat")
+
+//const math = new Math() // Nao precisar ser instanciada pq é static
+const num = Math.random() * 100
+mat.innerHTML = Math.sqrt(64)
+mat.innerHTML = Math.pow(10, 2) // Elevado 
+  
