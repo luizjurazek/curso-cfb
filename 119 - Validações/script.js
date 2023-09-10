08697ed8019808f89a871d18b00c75d26844dabb
Luiz Jurazek <luizjurazek@gmail.com>
2023-09-10 15:33:01 -0300

    Validações

---
+++ 119 - Validações/script.js
@@ -0,0 +1,19 @@
+const f_nome = document.querySelector("#f_nome");
+const f_nota = document.querySelector("#f_nota");
+const f_msg = document.querySelector("#f_msg");
+
+document.querySelector("#btn_validar").addEventListener("click",(evt)=>{
+    let msg = null;
+    let estadoValidacao = f_nome.validity
+
+    if(!f_nota.checkValidity()){
+        msg = f_nota.validationMessage
+    } 
+
+    if(estadoValidacao.valueMissing){
+        f_nome.setCustomValidity("Esse campo é obrigatório")
+    } 
+    f_msg.innerHTML = f_nome.validationMessage
+    f_msg.innerHTML = msg;
+    evt.preventDefault();
+})
