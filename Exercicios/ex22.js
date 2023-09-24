2573905f868d9d43a7c0324765d39edd83b1a92b
Luiz Jurazek <luizjurazek@gmail.com>
2023-09-24 19:56:09 -0300

    Ex22

--- Exercicios/ex22.js
+++ Exercicios/ex22.js
@@ -1,5 +1,11 @@
-// Escreva um programa que leia um número e exiba o seu módulo
-
-function moduloDoNum(num){
+function moduloDoNum(num) {
+    // Usando a função Math.abs para obter o valor absoluto do número
+    var modulo = Math.abs(num);
     
+    // Exibindo o resultado
+    console.log("O módulo de " + num + " é " + modulo);
 }
+
+// Exemplo de uso da função
+var numero = -5;
+moduloDoNum(numero); // Isso exibirá "O módulo de -5 é 5" no console
