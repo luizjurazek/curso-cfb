7c28460ff59ae5429ab49c120988804e3204f278
Luiz Jurazek <luizjurazek@gmail.com>
2023-09-28 21:39:13 -0300

    Deletar itens do grid e bd

--- 152 - DataGrid View/script.js
+++ 152 - DataGrid View/script.js
@@ -1,5 +1,5 @@
 const configDataGridView = {
-    endpoint: "produtos.json",
+    endpoint: "http://127.0.0.1:1880/produtos",
     idDestino: "dgvDados"
 }
 
@@ -7,54 +7,65 @@ const dataGridView = (config) => {
     const dgvDados = document.getElementById(config.idDestino)
     dgvDados.innerHTML = ""
     fetch(config.endpoint)
-    .then(res => res.json())
-    .then(res => {
-        res.forEach((el) => {
-            const dgvLinha = document.createElement("div")
-            dgvLinha.setAttribute("class", "dgvLinha")
+        .then(res => res.json())
+        .then(res => {
+            res.forEach((el) => {
+                const dgvLinha = document.createElement("div")
+                dgvLinha.setAttribute("class", "dgvLinha")
 
-            const c1 = document.createElement("div")
-            c1.setAttribute("class", "item c1")
-            c1.innerHTML = el.id
-            dgvLinha.appendChild(c1)
+                const c1 = document.createElement("div")
+                c1.setAttribute("class", "item c1")
+                c1.innerHTML = el.n_id_produto
+                dgvLinha.appendChild(c1)
 
-            const c2 = document.createElement("div")
-            c2.setAttribute("class", "item c2")
-            c2.innerHTML = el.produto
-            dgvLinha.appendChild(c2)
+                const c2 = document.createElement("div")
+                c2.setAttribute("class", "item c2")
+                c2.innerHTML = el.s_nome_produto
+                dgvLinha.appendChild(c2)
 
-            const c3 = document.createElement("div")
-            c3.setAttribute("class", "item c3")
-            c3.innerHTML = el.marca
-            dgvLinha.appendChild(c3)
+                const c3 = document.createElement("div")
+                c3.setAttribute("class", "item c3")
+                c3.innerHTML = el.s_marca_produto
+                dgvLinha.appendChild(c3)
 
-            const c4 = document.createElement("div")
-            c4.setAttribute("class", "item c4")
-            c4.innerHTML = el.modelo
-            dgvLinha.appendChild(c4)
+                const c4 = document.createElement("div")
+                c4.setAttribute("class", "item c4")
+                c4.innerHTML = el.s_modelo_produto
+                dgvLinha.appendChild(c4)
 
-            const iconDelete = document.createElement("img")
-            iconDelete.setAttribute("class", "dgvIcone")
-            iconDelete.setAttribute("src", "./icons/delete.svg")
+                const iconDelete = document.createElement("img")
+                iconDelete.setAttribute("class", "dgvIcone")
+                iconDelete.setAttribute("src", "./icons/delete.svg")
+                iconDelete.addEventListener("click", (evt) => {
+                    const id = evt.target.parentNode.parentNode.firstChild.innerHTML;
+                    const linha = evt.target.parentNode.parentNode;
+                    const endpoint = `http://127.0.0.1:1880/removeproduto/${id}`;
+                    fetch(endpoint)
+                        .then(res => {
+                            if(res.status == 200){
+                                linha.remove()
+                            }
+                        })
+                })
 
-            const iconEdit = document.createElement("img")
-            iconEdit.setAttribute("class", "dgvIcone")
-            iconEdit.setAttribute("src", "./icons/edit.svg")
+                const iconEdit = document.createElement("img")
+                iconEdit.setAttribute("class", "dgvIcone")
+                iconEdit.setAttribute("src", "./icons/edit.svg")
 
-            const iconView = document.createElement("img")
-            iconView.setAttribute("class", "dgvIcone")
-            iconView.setAttribute("src", "./icons/visibility.svg")
+                const iconView = document.createElement("img")
+                iconView.setAttribute("class", "dgvIcone")
+                iconView.setAttribute("src", "./icons/visibility.svg")
 
-            const c5 = document.createElement("div")
-            c5.setAttribute("class", "item c5")
-            c5.append(iconView, iconEdit, iconDelete)
-            dgvLinha.appendChild(c5)
+                const c5 = document.createElement("div")
+                c5.setAttribute("class", "item c5")
+                c5.append(iconView, iconEdit, iconDelete)
+                dgvLinha.appendChild(c5)
 
 
-            dgvDados.appendChild(dgvLinha)
+                dgvDados.appendChild(dgvLinha)
+            })
+            console.log(res)
         })
-        console.log(res)
-    })
 }
 
 dataGridView(configDataGridView)
