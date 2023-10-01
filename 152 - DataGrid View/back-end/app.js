f383378924e5aec6914be9af949fb3e7479882f9
Luiz Jurazek <luizjurazek@gmail.com>
2023-10-01 11:59:27 -0300

    Get produtos na tabela

--- 152 - DataGrid View/back-end/app.js
+++ 152 - DataGrid View/back-end/app.js
@@ -1,33 +1,37 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const dbConnection = require('./connection.js'); // Import the connection pool from db.js
 
+app.use(cors())
 const port = 3000
 
 // Now you can use dbConnection to interact with the database
-async function queryDatabase() {
-    try {
-        const [rows, fields] = await dbConnection.execute('SELECT * FROM produto');
-        console.log('Query result:', rows);
-    } catch (error) {
-        console.error('Error querying the database:', error);
-    }
-}
+// async function queryDatabase() {
+//     try {
+//         let [rows] = await dbConnection.execute('SELECT * FROM produto');
+//         return rows
+//         console.log('Query:', rows);
+//     } catch (error) {
+//         console.error('Error querying the database:', error);
+//     }
+// }
 
 // Get all products 
 async function getProdutos(){
     try {
         const [rows] = await dbConnection.execute('SELECT * FROM produto')
+        return rows
         // console.log("Produtos: " + rows)
     } catch (error){
         console.log("Error: " + error)
     }
 }
 
-app.get('/', async (req, res) => {
-    const query = await queryDatabase()
-    res.send(query)
-})
+// app.get('/', async (req, res) => {
+//     const query = await queryDatabase()
+//     res.send(query)
+// })
 
 app.get('/produtos', async (req, res) => {
     const query = await getProdutos()
