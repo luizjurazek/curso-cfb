d2a9deb81c60bd37179477b12fecea858836c839
Luiz Jurazek <luizjurazek@gmail.com>
2023-11-17 20:31:44 -0300

    Exercicios

---
+++ Exercicios/ex42.js
@@ -0,0 +1,4 @@
+// Programa que imprime números de 100 a 1 em ordem decrescente
+for (let i = 100; i >= 1; i--) {
+    console.log(i);
+}
---
+++ Exercicios/ex43.js
@@ -0,0 +1,4 @@
+// Programa que imprime números pares de 1 a 100
+for (let i = 2; i <= 100; i += 2) {
+    console.log(i);
+}
